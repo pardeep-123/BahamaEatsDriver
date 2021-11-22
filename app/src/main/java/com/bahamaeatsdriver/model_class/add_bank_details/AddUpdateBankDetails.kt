c8ee94130d53f386bb35658de7179bcf24a10a9e
Poonam.cqlsys <Poonam@cqlsys.co.uk>
2021-11-22 18:48:13 +0530

    Update driver payout details done

---
+++ app/src/main/java/com/bahamaeatsdriver/model_class/add_bank_details/AddUpdateBankDetails.kt
@@ -0,0 +1,48 @@
+package com.bahamaeatsdriver.model_class.add_bank_details
+
+import com.google.gson.annotations.SerializedName
+
+
+data class AddUpdateBankDetails(
+    @SerializedName("body")
+    val body: Body,
+    @SerializedName("code")
+    val code: Int,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("status")
+    val status: Boolean
+)
+
+data class Body(
+    @SerializedName("account_holder_name")
+    val accountHolderName: String,
+    @SerializedName("account_number")
+    val accountNumber: String,
+    @SerializedName("account_type")
+    val accountType: String,
+    @SerializedName("bank_name")
+    val bankName: String,
+    @SerializedName("branch_name")
+    val branchName: String,
+    @SerializedName("createdAt")
+    val createdAt: String,
+    @SerializedName("dob")
+    val dob: String,
+    @SerializedName("driver_id")
+    val driverId: Int,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("first_name")
+    val firstName: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("is_verified")
+    val isVerified: Int,
+    @SerializedName("last_name")
+    val lastName: String,
+    @SerializedName("nib_number")
+    val nibNumber: String,
+    @SerializedName("updatedAt")
+    val updatedAt: String
+)
